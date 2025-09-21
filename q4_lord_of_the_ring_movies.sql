86e2ee70d8a942735fca08d9a86a53872955eb9e
Simin <simin.asadzadeh7@gmail.com>
2025-09-21 18:29:08 -0400

    Update q4_lord_of_the_ring_movies.sql

--- q4_lord_of_the_ring_movies.sql
+++ q4_lord_of_the_ring_movies.sql
@@ -1,1 +1,13 @@
 -- List the movies that include "The Lord of the Rings"
+
+SELECT title, release_date
+FROM movies
+WHERE title LIKE '%The Lord of the Rings%'
+ORDER BY release_date ASC;
+
+-- Answer:
+-- The Lord of the Rings|1978-11-15
+-- The Lord of the Rings: The Fellowship of the Ring|2001-12-18
+-- The Lord of the Rings: The Two Towers|2002-12-18
+-- The Lord of the Rings: The Return of the King|2003-12-17
+-- The Lord of the Rings: The War of the Rohirrim|2024-12-05
