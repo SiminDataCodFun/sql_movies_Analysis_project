164ae2a967d43e234654a4ca74606a28eb5f3390
Simin <simin.asadzadeh7@gmail.com>
2025-09-21 18:22:02 -0400

    Update q3_top_5_most_popular_2024.sql

--- q3_top_5_most_popular_2024.sql
+++ q3_top_5_most_popular_2024.sql
@@ -1,1 +1,15 @@
 -- What are the 5 most popular movies released in 2024
+
+SELECT title, popularity
+FROM movies
+where strftime('%Y', release_date) = '2024'
+ORDER BY popularity DESC
+LIMIT 5;
+
+-- Answer:
+-- Moana 2|52.033
+-- Sonic the Hedgehog 3|43.9503
+-- Despicable Me 4|38.7004
+-- Deadpool & Wolverine|38.1015
+-- Inside Out 2|36.0925
+
