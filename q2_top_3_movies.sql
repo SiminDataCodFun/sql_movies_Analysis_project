9b1f4b6274e685a1f88f565fc7e799750ce274ba
Simin <simin.asadzadeh7@gmail.com>
2025-09-21 18:13:26 -0400

    Update q2_top_3_movies.sql

--- q2_top_3_movies.sql
+++ q2_top_3_movies.sql
@@ -1,6 +1,11 @@
 -- What are the 3 movies with the most votes of all times
 
-SELECT title 
+SELECT title, vote_count
 from movies
 ORDER BY vote_count DESC
 LIMIT 3; 
+
+-- Answer:
+-- Inception|37773
+-- Interstellar|37616
+-- The Dark Knight|34200
